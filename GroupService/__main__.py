03b5480f2840ab28981ce134b1b0992b093baaea
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-09-14 18:03:58 +0530

    ***

--- GroupService/__main__.py
+++ GroupService/__main__.py
@@ -207,7 +207,7 @@ def start(update: Update, context: CallbackContext):
     first_name = update.effective_user.first_name
     
     # Send the START_IMG first
-    elif START_IMG:
+    if START_IMG:
         update.effective_message.reply_photo(
             START_IMG,  # URL or file path for the image
             caption=PM_START_TEXT.format(escape_markdown(first_name), "BOT_NAME"),
